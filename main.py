9d3311aa70efe4d681c785c67507f32168f79ea2
KinfThaDerp <bassam.grzegorz@gmail.com>
2025-11-25 14:37:45 +0100

    feat: login logout buttons test

--- main.py
+++ main.py
@@ -1,37 +1,46 @@
-import tkinter, tkintermapview
+import tkinter as tk
+import tkintermapview
 from libmap import controller as ctrl
 from libmap import model
 
 availableStates = ("Disable","Login", "Map")
 appState = availableStates[1]
 
+
+
 def logIn(window):
+    global appState
+    appstate = availableStates[2]
     window.destroy()
 
+def logOut(window):
+    global appState
+    appstate = availableStates[1]
+    window.destroy()
 
 def loginWindow():
-    root_log = tkinter.Tk()
+    root_log = tk.Tk()
     root_log.title("Okno Logowania")
     root_log.geometry("300x300")
 
-    button_login = tkinter.Button(root_log, text="Login", command=lambda: print("Loggin in..."))
+    button_login = tk.Button(root_log, text="Login", command=lambda: logIn(root_log))
     button_login.pack()
 
     root_log.mainloop()
     global appState
-    appState = availableStates[2]
+    appState = availableStates[0]
 
 def mapWindow():
-    root_map = tkinter.Tk()
+    root_map = tk.Tk()
     root_map.title("Okno Map")
     root_map.geometry("1280x720")
 
-    button_logot = tkinter.Button(root_map, text="Log out", command=lambda: print("Loggin out..."))
+    button_logot = tk.Button(root_map, text="Log out", command=lambda: logOut(root_map))
     button_logot.pack()
 
     root_map.mainloop()
     global appState
-    appState = availableStates[1]
+    appState = availableStates[0]
 
 while True:
     if appState == availableStates[0]:
