ca12f85c82d461083f401bf0eb7429b4245c6baf
KinfThaDerp <bassam.grzegorz@gmail.com>
2025-12-01 12:46:39 +0100

    feat: ContactData class with email and number validation

--- libmap/model.py
+++ libmap/model.py
@@ -1,15 +1,48 @@
-# class Library:
-#     def __init__(self):
-#         self.name = ""
-#
-# class City:
-#     def __init__(self):
-#         self.name = ""
-#
-# class LibraryWorker:
-#     def __init__(self):
-#         self.name = ""
-#
-# class LibraryReader:
-#     def __init__(self):
-#         self.name = ""
+import re
+
+class ContactData:
+    def __init__(self, number:int, email:str):
+        self.phoneNumber = number
+        self.checkValidNumber()
+        self.email = email
+        self.checkValidEmail()
+    def checkValidNumber(self):
+        if (len(str(self.phoneNumber)) == 9) and isinstance(self.phoneNumber, int):
+            return True
+        else:
+            raise Exception("Invalid Phone Number!")
+    def checkValidEmail(self):
+        if isinstance(self.email, str) and re.match(r"[^@]+@[^@]+\.[^@]+", self.email):
+            return self.email
+        else:
+            raise Exception("Invalid Email!")
+
+
+class Library:
+    def __init__(self, name, address, coords, phonenumber, email):
+        self.name = name
+        self.address = address
+        self.coords = coords
+        self.contact = ContactData(phonenumber, email)
+
+class City:
+    def __init__(self):
+        self.name = ""
+
+class Person:
+    def __init__(self, name:str, surname:str):
+        self.name = name
+        self.surname = surname
+
+class LibraryWorker(Person):
+    def __init__(self, name:str, surname:str):
+        super().__init__(name, surname)
+
+class LibraryReader(Person):
+    def __init__(self, name:str, surname:str):
+        super().__init__(name, surname)
+
+if __name__ == '__main__':
+
+    ziemniak = ContactData(123456789,"bassam.grzegorz@gmail.com")
+    print(ziemniak.phoneNumber, ziemniak.email)
--- main.py
+++ main.py
@@ -10,15 +10,17 @@ appState = availableStates[1]
 
 def logIn(window):
     global appState
-    appstate = availableStates[2]
+    appState = availableStates[2]
     window.destroy()
 
 def logOut(window):
     global appState
-    appstate = availableStates[1]
+    appState = availableStates[1]
     window.destroy()
 
 def loginWindow():
+    global appState
+    appState = availableStates[0]
     root_log = tk.Tk()
     root_log.title("Okno Logowania")
     root_log.geometry("300x300")
@@ -27,10 +29,11 @@ def loginWindow():
     button_login.pack()
 
     root_log.mainloop()
-    global appState
-    appState = availableStates[0]
+
 
 def mapWindow():
+    global appState
+    appState = availableStates[0]
     root_map = tk.Tk()
     root_map.title("Okno Map")
     root_map.geometry("1280x720")
@@ -39,13 +42,10 @@ def mapWindow():
     button_logot.pack()
 
     root_map.mainloop()
-    global appState
-    appState = availableStates[0]
 
-while True:
-    if appState == availableStates[0]:
-        break
-    elif appState == availableStates[1]:
+
+while appState != availableStates[0]:
+    if appState == availableStates[1]:
         loginWindow()
     elif appState == availableStates[2]:
         mapWindow()
