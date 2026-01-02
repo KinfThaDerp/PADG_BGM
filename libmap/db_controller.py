579c7abe102976c31ba146d2e4116ccae70804b4
KinfThaDerp <bassam.grzegorz@gmail.com>
2026-01-02 15:42:01 +0100

    feat: database credentials read from .env file

--- libmap/db_controller.py
+++ libmap/db_controller.py
@@ -1,11 +1,16 @@
+import os
 import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv(verbose=True)
 
 db_connection = psycopg2.connect(
-    "dbname=postgres",
-    user="postgres",
-    password="postgres",
-    host="localhost",
-    port=5432)
+    dbname=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USERNAME"),
+    password=os.getenv("DB_PASSWORD"),
+    host=os.getenv("DB_HOST"),
+    port=os.getenv("DB_PORT")
+)
 
 db_cursor = db_connection.cursor()
 
